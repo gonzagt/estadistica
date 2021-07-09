9ac6a058f67c05cf33e60c371ddd13abbe65fdcd
Gonzalo <gonzalogermantello@gmail.com>
2021-07-08 22:23:33 -0300

    Update con la función calcularMediana() 100% funcional y que organiza las listas numéricas de menor a mayor antes de calcular el resultado final.

--- mediana.js
+++ mediana.js
@@ -12,12 +12,13 @@ const lista1 = [
     400,
     500,
     800,
-    1200
+    1200,
 ];
 
-// Primero que nada, debemos saber si nuestra lista es par o impar.
-// Para ello, creamos una función que nos ayude con esto.
-// La función esPar(n), nos va a indicar si n es 0, par o impar.
+// Primero que nada, debemos saber si nuestra lista es par
+// o impar. Para ello, creamos una función que nos ayude con 
+// esto. La función esPar(n), nos va a indicar si n es 0, par
+// o impar.
 
 function esPar (n) {
     if (n === 0) {
@@ -29,11 +30,11 @@ function esPar (n) {
     }
 }
 
-// Creamos nuestra "calculadora de mediana" de esa lista
-
-const mitadLista1 = parseInt(lista1.length/2);
+// Creamos nuestra "calculadora de mediana" de esa lista.
 
 let mediana1;
+
+const mitadLista1 = parseInt(lista1.length/2);
 
 if (esPar(lista1.length)) {
     const e1 = lista1[mitadLista1 - 1];
@@ -46,8 +47,38 @@ if (esPar(lista1.length)) {
 // Creamos otra forma de "calcular la mediana".
 // Nuestra "lista" debe ser un array con números.
 
-function calcularMediana(lista) {
+function calcularMedianaOrdenada(lista) {
     
+    let mediana;
+    const mitadLista = parseInt(lista.length / 2);
+    if (esPar(lista.length)) {
+        const e1 = lista[mitadLista - 1];
+        const e2 = lista[mitadLista];
+        mediana = (e1 + e2) / 2;
+    } else {
+        mediana = lista[mitadLista];
+    }
+    return mediana;
+};
+
+// Estos métodos, funcionaron para listas ordenadas, pero no
+// siempre podemos esperar listas ordenadas. Para solucionar
+// esto, vamos a usar un método que nos ordene los números
+// de menor a mayor: El método Array.sort(). Funciona así:
+
+// lista.sort(function (a, b) {
+//     return a - b;
+// });
+
+// Y ahora, vamos a cambiar nuestra función calcularMediana()
+// para que ordene los elementos de la lista antes de que nos
+// retorne una solución, y evitar así problemas de desorden de 
+// elementos.
+
+function calcularMediana(lista) {
+    lista.sort(function (a, b) {
+        return a - b;
+    });
     let mediana;
     const mitadLista = parseInt(lista.length / 2);
     if (esPar(lista.length)) {
